acf833f54e5549f32dd8b7d23f4b61ce2dabb23d
PedroPK <pedropk@gmail.com>
2015-11-04 12:22:20 -0300

    Tela de Consulta funcionando, com DataTable

--- UnibratecPSCJpaWeb/src/br/unibratec/web/jsf/managedbeans/PassageiroManagedBean.java
+++ UnibratecPSCJpaWeb/src/br/unibratec/web/jsf/managedbeans/PassageiroManagedBean.java
@@ -1,15 +1,19 @@
 package br.unibratec.web.jsf.managedbeans;
 
+import java.util.Collection;
+
 import javax.faces.bean.ManagedBean;
 
 import br.unibratec.linhasaereas.entidades.Passageiro;
 import br.unibratec.linhasaereas.fachada.FachadaLinhasAereas;
 import br.unibratec.linhasaereas.fachada.IFachadaLinhasAereas;
+import br.unibratec.util.BibliotecaFuncoes;
 
 @ManagedBean
 public class PassageiroManagedBean {
 	
 	private Passageiro aPassageiro;
+	private Collection<Passageiro> aColecaoPassageiros;
 	
 	public Passageiro getPassageiro() {
 		if ( this.aPassageiro == null ) {
@@ -17,16 +21,31 @@ public class PassageiroManagedBean {
 		}
 		return this.aPassageiro;
 	}
-	
 	public void setPassageiro(Passageiro pPassageiro) {
 		this.aPassageiro = pPassageiro;
 	}
 	
+	public Collection<Passageiro> getColecaoPassageiros() {
+		if ( this.aColecaoPassageiros == null  /*!BibliotecaFuncoes.isColecaoValida(this.aColecaoPassageiros)*/ ) {
+			this.consultarGeral();
+		}
+		return aColecaoPassageiros;
+	}
+	public void setColecaoPassageiros(Collection<Passageiro> pColecaoPassageiros) {
+		this.aColecaoPassageiros = pColecaoPassageiros;
+	}
+	
 	public void inserir() {
-		if ( this.aPassageiro != null ) {
+		if ( this.aPassageiro != null && this.aPassageiro.getChavePrimaria() != null ) {
 			IFachadaLinhasAereas fachada = new FachadaLinhasAereas();
 			fachada.inserir(this.aPassageiro);
 		}
+	}
+	
+	public Collection<Passageiro> consultarGeral() {
+		IFachadaLinhasAereas fachada = new FachadaLinhasAereas();
+		this.aColecaoPassageiros = fachada.consultar();
+		return this.aColecaoPassageiros;
 	}
 	
 }
