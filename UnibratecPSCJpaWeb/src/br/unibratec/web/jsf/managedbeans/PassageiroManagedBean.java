0975aaca8977174771c9766a908e8e1bfc53b3d9
pedropk <pedropk@172.20.10.2>
2015-11-04 18:50:45 -0300

    Método e atributo para Consultar o cadastro de Passageiros

--- UnibratecPSCJpaWeb/src/br/unibratec/web/jsf/managedbeans/PassageiroManagedBean.java
+++ UnibratecPSCJpaWeb/src/br/unibratec/web/jsf/managedbeans/PassageiroManagedBean.java
@@ -1,5 +1,7 @@
 package br.unibratec.web.jsf.managedbeans;
 
+import java.util.Collection;
+
 import javax.faces.bean.ManagedBean;
 
 import br.unibratec.linhasaereas.entidades.Passageiro;
@@ -10,6 +12,7 @@ import br.unibratec.linhasaereas.fachada.IFachadaLinhasAereas;
 public class PassageiroManagedBean {
 	
 	private Passageiro aPassageiro;
+	private Collection<Passageiro> aColecaoPassageiros;
 	
 	public Passageiro getPassageiro() {
 		if ( this.aPassageiro == null ) {
@@ -22,11 +25,27 @@ public class PassageiroManagedBean {
 		this.aPassageiro = pPassageiro;
 	}
 	
+	public Collection<Passageiro> getColecaoPassageiros() {
+		if ( this.aColecaoPassageiros == null  /*!BibliotecaFuncoes.isColecaoValida(this.aColecaoPassageiros)*/ ) {
+			this.consultarGeral();
+		}
+		return aColecaoPassageiros;
+	}
+	public void setColecaoPassageiros(Collection<Passageiro> pColecaoPassageiros) {
+		this.aColecaoPassageiros = pColecaoPassageiros;
+	}
+	
 	public void inserir() {
 		if ( this.aPassageiro != null ) {
 			IFachadaLinhasAereas fachada = new FachadaLinhasAereas();
 			fachada.inserir(this.aPassageiro);
 		}
+	}
+	
+	public Collection<Passageiro> consultarGeral() {
+		IFachadaLinhasAereas fachada = new FachadaLinhasAereas();
+		this.aColecaoPassageiros = fachada.consultar();
+		return this.aColecaoPassageiros;
 	}
 	
 }
