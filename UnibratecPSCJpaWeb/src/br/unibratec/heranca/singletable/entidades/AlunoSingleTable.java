1db9b44062a56e28c2361f203cf02cc33a95054c
pedropk <pedropk@192.168.25.211>
2015-11-10 04:25:23 -0300

    Ajustes de Identação

--- UnibratecPSCJpaWeb/src/br/unibratec/heranca/singletable/entidades/AlunoSingleTable.java
+++ UnibratecPSCJpaWeb/src/br/unibratec/heranca/singletable/entidades/AlunoSingleTable.java
@@ -10,11 +10,11 @@ public class AlunoSingleTable extends PessoaSingleTable {
 	private Boolean isFies;
 	
 	public AlunoSingleTable() {}
-
+	
 	public Boolean getIsFies() {
 		return isFies;
 	}
-
+	
 	public void setIsFies(Boolean pIsFies) {
 		this.isFies = pIsFies;
 	}
